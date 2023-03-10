58ff593bdd845e6067d7b284d357b3bb8c11f443
Davin <71451687+bc-davin@users.noreply.github.com>
2023-03-10 06:17:59 -0700

    AddCityTest

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -3,27 +3,21 @@ package com.example.lab8;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
 //import org.junit.Before;
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
 public class CustomListTest {
-    private CustomList mockCityList(){
-        CustomList customList = new CustomList(null, new ArrayList<City>());
-        customList.addCity(mockCity());
-        return customList;
-    }
-    private City mockCity(){
-        City city = new City("Toronto", "Ontario");
-        return city;
-    }
+    private ArrayList<City> cityDataList= new ArrayList<>();
     @Test
-    public void testGetCount(){
-        CustomList customList = mockCityList();
-        assertEquals(1, customList.getCount());
+    public void addCityTest(){
+        CustomList customList = new CustomList(null,cityDataList);
+        City city = new City("Edmonton","AB");
+        customList.addCity(city);
+        assertEquals(1,customList.getCount());
     }
 
 }
